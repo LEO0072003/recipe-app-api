d290929f946a86d0121533acc50fea908a692150
LEO0072003 <pradyumnaghoshal@gmail.com>
2022-06-13 12:40:14 +0530

    Updated code

--- app/core/models.py
+++ app/core/models.py
@@ -12,9 +12,9 @@ from django.contrib.auth.models import(
 class UserManager(BaseUserManager):
     """manager for users """
 
-    def create_user(name, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         """Create save and return a new user """
-        user = self.model(email=email, **extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
