eeb27ea4af2b94bcfb1d90ff078b2563df7f2021
LEO0072003 <pradyumnaghoshal@gmail.com>
2022-06-27 11:25:46 +0530

    Updated Code

--- Dockerfile
+++ Dockerfile
@@ -15,6 +15,7 @@ RUN python -m venv /py && \
     apk add --update --no-cache postgresql-client && \
     apk add --update --no-cache --virtual .tmp-build-deps \
         build-base postgresql-dev musl-dev && \
+    /py/bin/pip install tk && \
     /py/bin/pip install -r /tmp/requirements.txt && \
     /py/bin/pip install tk &&\
     if [ $DEV = "true" ]; \
--- app/user/views.py
+++ app/user/views.py
@@ -2,7 +2,7 @@
 Views for the user API
 
 """
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions, status
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
@@ -16,7 +16,19 @@ class CreateUserView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
+
 class CreatTokenView(ObtainAuthToken):
     """create a new auth token for user"""
     serializer_class = AuthTokenSerializer
-    renderer_classes = api_settings.DEFAULT_RENDERER_CLASS
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """retrieve and return the authenticated user"""
+        return self.request.user
